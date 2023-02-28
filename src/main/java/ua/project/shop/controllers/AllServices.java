399573ca59bd71dd4bc7632e35a20adde737d883
Oleksandr Fedoniuk <malish196sm@gmail.com>
2023-02-28 12:51:24 +0200

    getters & adding for all tables

--- src/main/java/ua/project/shop/controllers/AllServices.java
+++ src/main/java/ua/project/shop/controllers/AllServices.java
@@ -2,8 +2,7 @@ package ua.project.shop.controllers;
 
 import org.springframework.stereotype.Service;
 import ua.project.shop.repositories.*;
-import ua.project.shop.tables.Customer;
-import ua.project.shop.tables.Product;
+import ua.project.shop.tables.*;
 
 import java.util.List;
 
@@ -32,12 +31,28 @@ public class AllServices {
     public List<Customer> getCustomers(){
         return customerRepository.findAll();
     }
-
+    public List<Category> getCategories(){
+        return categoryRepository.findAll();
+    }
+    public List<Brand> getBrand(){
+        return brandRepository.findAll();
+    }
+    public List<Orders> getOrders(){
+        return ordersRepository.findAll();
+    }
     public void addProduct(Product product) {
         productRepository.save(product);
     }
-
     public void addCustomer(Customer customer) {
         customerRepository.save(customer);
+    }
+    public void addCategory(Category category) {
+        categoryRepository.save(category);
+    }
+    public void addBrand(Brand brand) {
+        brandRepository.save(brand);
+    }
+    public void addOrders(Orders orders) {
+        ordersRepository.save(orders);
     }
 }
